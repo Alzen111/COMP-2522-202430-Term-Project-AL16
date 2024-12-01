35c27bc6f02bdf1f102c2554570d54ccf9ed6b2f
Alzen111 <alzenlandayan1616@gmail.com>
2024-12-01 05:40:22 -0800

    Implement HUD class to show level

--- core/src/main/java/korin/climb/Scenes/Hud.java
+++ core/src/main/java/korin/climb/Scenes/Hud.java
@@ -4,39 +4,53 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import korin.climb.KorinClimb;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.graphics.Color;
 
-
-public class Hud {
+/**
+ * Displays the game's HUD, including the current level.
+ */
+public class Hud implements Disposable {
     public Stage stage;
-    private Viewport viewport;
 
-    Label scoreLabel;
-    Label strScore;
+    Label levelNum;
+    Label level;
 
+    /**
+     * Create the HUD and set up labels.
+     *
+     * @param sb SpriteBatch for drawing
+     */
     public Hud(SpriteBatch sb) {
 
-        Integer score = 0;
-
-        viewport = new FitViewport(KorinClimb.V_WIDTH, KorinClimb.V_HEIGTH, new OrthographicCamera());
+        Viewport viewport = new FitViewport(KorinClimb.V_WIDTH, KorinClimb.V_HEIGTH, new OrthographicCamera());
         stage = new Stage(viewport, sb);
 
         Table table = new Table();
         table.top();
         table.setFillParent(true);
 
-        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE ));
-        strScore = new Label("Score", new Label.LabelStyle(new BitmapFont(), Color.WHITE ));
+        levelNum = new Label("1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        level = new Label("LEVEL", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
-        table.add(strScore).expandX().padTop(10);
+        table.add(level).expandX().padTop(10);
         table.row();
-        table.add(scoreLabel).expandX().padTop(10);
+        table.add(levelNum).expandX().padTop(2);
 
         stage.addActor(table);
     }
+
+    /**
+     * Cleans up resources.
+     */
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
 }
